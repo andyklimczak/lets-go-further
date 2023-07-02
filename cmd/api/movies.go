a80117ce7717650c5240070275e0bd7777a01d96
Andy Klimczak <andyklimczak@fastmail.com>
2023-07-02 20:34:48 +0100

    ch04

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,16 +6,43 @@ import (
   "time"
 
   "greenlight.alexedwards.net/internal/data"
+  "greenlight.alexedwards.net/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "create a new movie")
+  var input struct {
+    Title string `json:"title"`
+    Year int32 `json:"year"`
+    Runtime data.Runtime `json:"runtime"`
+    Genres []string `json:"genres"`
+  }
+
+  err := app.readJSON(w, r, &input)
+  if err != nil {
+    app.errorResponse(w, r,http.StatusBadRequest, err.Error())
+    return
+  }
+  movie := &data.Movie{
+    Title: input.Title,
+    Year: input.Year,
+    Runtime: input.Runtime,
+    Genres: input.Genres,
+  }
+
+  v := validator.New()
+
+  if data.ValidateMovie(v, movie); !v.Valid() {
+    app.failedValidationResponse(w, r, v.Errors)
+    return
+  }
+
+  fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
   id, err := app.readIDParam(r)
   if err != nil {
-    app.notFoundResponse(w, r)
+    app.badRequestResponse(w, r, err)
     return
   }
 
